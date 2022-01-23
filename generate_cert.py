7d60579c11b17b7d211d1244b9f7953fbdbff77d
purplepig4657 <purplepig4657@gmail.com>
2022-01-23 20:20:32 +0900

    ip write error modify

--- generate_cert.py
+++ generate_cert.py
@@ -2,7 +2,7 @@ import hashlib
 import os
 
 
-def make_config_file(ip_hash, name):
+def make_config_file(ip, ip_hash, name):
     with open(f'./cert/{ip_hash}.csr.cnf', 'w') as csr_cnf:
         content = f'''
             default_bits = 2048
@@ -15,9 +15,9 @@ def make_config_file(ip_hash, name):
             ST=CC
             L=DJ
             O=UO
-            OU={ip_hash}
+            OU={ip}
             emailAddress={name}.email.com
-            CN={ip_hash}
+            CN={ip}
         '''
         csr_cnf.write(content)
 
@@ -29,7 +29,7 @@ def make_config_file(ip_hash, name):
             subjectAltName = @alt_names
 
             [alt_names]
-            IP.1 = {ip_hash}
+            IP.1 = {ip}
         '''
         ext.write(content)
 
@@ -39,7 +39,7 @@ def generate_key_and_crt_file(json):
     ip = json['ip']
     ip_hash = hashlib.sha256(ip.encode()).hexdigest()
 
-    make_config_file(ip_hash, name)
+    make_config_file(ip, ip_hash, name)
 
     os.system('openssl genrsa -out server.key 2048 ')
     os.system(f'openssl req -new -sha256 -nodes -out server.csr newkey rsa:2048 -keyout server.key config <( cat {ip_hash}.csr.cnf )')
