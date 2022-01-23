9d4f7eb4c5a77867dba1cd44ed0fb08aacbdc891
purplepig4657 <purplepig4657@gmail.com>
2022-01-24 00:09:08 +0900

    change shell script and add chmod

--- generate_cert.py
+++ generate_cert.py
@@ -3,8 +3,9 @@ import os
 
 
 def make_config_file(ip, ip_hash, name):
-    with open(f'./cert/{ip_hash}.csr.cnf', 'w') as csr_cnf:
+    with open(f'./cert/{ip_hash}.conf', 'w') as conf:
         content = f'''
+            [req]
             default_bits = 2048
             prompt = no
             default_md = sha256
@@ -18,27 +19,25 @@ def make_config_file(ip, ip_hash, name):
             OU={ip}
             emailAddress={name}.email.com
             CN={ip}
-        '''
-        csr_cnf.write(content)
-
-    with open(f'./cert/{ip_hash}.ext', 'w') as ext:
-        content = f'''
+            
+            
+            [req_ext]
             authorityKeyIdentifier=keyid,issuer
             basicConstraints=CA:FALSE
             keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
             subjectAltName = @alt_names
-
+            
             [alt_names]
             IP.1 = {ip}
         '''
-        ext.write(content)
+        conf.write(content)
 
 
 def generate_sh_file(ip_hash):
     commands = f'''#!/bin/bash
     openssl genrsa -out ./cert/{ip_hash}.key 2048
-    openssl req -new -sha256 -nodes -out ./cert/{ip_hash}.csr newkey rsa:2048 -keyout ./cert/{ip_hash}.key -config <( cat ./cert/{ip_hash}.csr.cnf )
-    openssl x509 -req -in ./cert/{ip_hash}.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out ./cert/{ip_hash}.crt -days 500 -sha256 -extfile ./cert/{ip_hash}.ext
+    openssl req -sha256 -out ./cert/{ip_hash}.csr -key ./cert/{ip_hash}.key -config ./cert/{ip_hash}.conf
+    openssl x509 -req -in ./cert/{ip_hash}.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out ./cert/{ip_hash}.crt -days 500 -sha256 -extfile ./cert/{ip_hash}.conf -extensions req_ext
     '''
 
     with open(f'./{ip_hash}.sh', 'w') as sh:
@@ -53,4 +52,5 @@ def generate_key_and_crt_file(json):
     make_config_file(ip, ip_hash, name)
     generate_sh_file(ip_hash)
 
+    os.chmod(f'{ip_hash}', 0o755)
     os.system(f'./{ip_hash}.sh')
